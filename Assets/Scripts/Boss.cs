c99a08df3af399ac2aad6faaa979c18566a0f02e
Baconchicken42 <darthsidious@earthlink.net>
2026-01-31 15:07:15 -0500

    public editor events + attack scaffold

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Boss : MonoBehaviour
 {
+    [Tooltip("References")]
+    Transform patternSpawnAnchor;
 
     [Tooltip("Stats")]
     public int health = 10000;
     public int phase2TransitionHealth = 6500;
     public int phase3TransitionHealth = 4000;
+
+    [Tooltip("Events")]
+    public UnityEvent onTakeDamage;
+    public UnityEvent onAttack;
+    public UnityEvent onEnterPhase2;
+    public UnityEvent onEnterPhase3;
+    public UnityEvent onDeath;
 
 
     private int phase = 1;
@@ -39,10 +49,16 @@ public class Boss : MonoBehaviour
         
     }
 
+    private void spawnBulletPattern(GameObject pattern)
+    {
+        Instantiate(pattern, patternSpawnAnchor.position, patternSpawnAnchor.rotation);
+    }
+
 
     public void takeDamage(int dmg)
     {
         health -= dmg;
+        onTakeDamage.Invoke();
         Debug.Log("Boss Health Remaining: " + health);
 
         if (phase == 1 && health <= phase2TransitionHealth)
@@ -56,18 +72,21 @@ public class Boss : MonoBehaviour
     private void enterPhase2()
     {
         phase = 2;
+        onEnterPhase2.Invoke();
         //TODO: Trigger Animations and Stuff
     }
 
     private void enterPhase3()
     {
         phase = 3;
+        onEnterPhase3.Invoke();
         //TODO: Trigger Animations and Stuff
     }
 
     private void die()
     {
         Debug.Log("Boss Killed");
+        onDeath.Invoke();
         Destroy(gameObject);
     }
 }
