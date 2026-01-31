dcf6b3d0ffef567d302e164982e2cd6d867a40f5
Baconchicken42 <darthsidious@earthlink.net>
2026-01-31 15:01:40 -0500

    basic phase transition structure

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -5,6 +5,8 @@ public class Boss : MonoBehaviour
 
     [Tooltip("Stats")]
     public int health = 10000;
+    public int phase2TransitionHealth = 6500;
+    public int phase3TransitionHealth = 4000;
 
 
     private int phase = 1;
@@ -14,11 +16,26 @@ public class Boss : MonoBehaviour
     void Start()
     {
         gm = FindFirstObjectByType<GameManager>();
+        if (!gm)
+            Debug.LogWarning("Boss: No Game Manager found in scene");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //TODO: shooting logic, animations?
+        if (phase == 1)
+        {
+
+        }
+        else if (phase == 2)
+        {
+
+        }
+        else if (phase == 3)
+        {
+
+        }
         
     }
 
@@ -27,9 +44,28 @@ public class Boss : MonoBehaviour
     {
         health -= dmg;
         Debug.Log("Boss Health Remaining: " + health);
+
+        if (phase == 1 && health <= phase2TransitionHealth)
+            enterPhase2();
+        else if (phase == 2 && health <= phase3TransitionHealth)
+            enterPhase3();
+        else if (health <= 0)
+            die();
     }
 
-    public void die()
+    private void enterPhase2()
+    {
+        phase = 2;
+        //TODO: Trigger Animations and Stuff
+    }
+
+    private void enterPhase3()
+    {
+        phase = 3;
+        //TODO: Trigger Animations and Stuff
+    }
+
+    private void die()
     {
         Debug.Log("Boss Killed");
         Destroy(gameObject);
